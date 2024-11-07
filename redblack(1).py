575fcf10c855a6eba293f8c7ffca300f1dea979a
Zoe Sztandke <zoe.sztandke@outlook.com>
2024-11-07 13:20:54 +0000

    fixed formatting and got rid of TODOs

--- redblack(1).py
+++ redblack(1).py
@@ -1,4 +1,3 @@
-
 # File:     redblack.py
 # Author:   John Longley
 # Date:     October 2024
@@ -10,46 +9,56 @@
 
 from enum import Enum
 
-Colour = Enum('Colour', ['Red','Black'])
+Colour = Enum('Colour', ['Red', 'Black'])
 Red, Black = Colour.Red, Colour.Black
 
-def colourStr(c):
-    return 'R' if c==Red else 'B'
 
-Dir = Enum('Dir', ['Left','Right'])
+def colourStr(c):
+    return 'R' if c == Red else 'B'
+
+
+Dir = Enum('Dir', ['Left', 'Right'])
 Left, Right = Dir.Left, Dir.Right
 
+
 def opposite(d):
-    if d==Left: return Right
-    else: return Left
+    if d == Left:
+        return Right
+    else:
+        return Left
+
 
 def branchLabel(d):
-    if d==Left: return 'l'
-    else: return 'r'
+    if d == Left:
+        return 'l'
+    else:
+        return 'r'
+
 
 class Node():
-    
-    def __init__(self,key,value):
+
+    def __init__(self, key, value):
         self.key = key
         self.value = value
         self.colour = Red
         self.left = None
         self.right = None
-        
-    def getChild(self,branch):
-        if branch==Left:
+
+    def getChild(self, branch):
+        if branch == Left:
             return self.left
         else:
             return self.right
 
-    def setChild(self,branch,y):
-        if branch==Left:
+    def setChild(self, branch, y):
+        if branch == Left:
             self.left = y
         else:
             self.right = y
 
     def __repr__(self):
-        return str(self.key) +':'+ str(self.value) +':'+ colourStr(self.colour)
+        return str(self.key) + ':' + str(self.value) + ':' + colourStr(self.colour)
+
 
 # Use None for all trivial leaf nodes
 
@@ -62,12 +71,10 @@ def colourOf(x):
 
 class RedBlackTree():
 
-    def __init__ (self):
+    def __init__(self):
         self.root = None
         self.stack = []
 
-
-# TODO: Task 1.
     def lookup(self, key):
         x = self.root
         while x is not None:
@@ -79,7 +86,6 @@ class RedBlackTree():
                 x = x.right
         return None
 
-# TODO: Task 2.
     def plainInsert(self, key, value):
         # check if key already exists in tree / tree DNE
         # if key already exists, overwrite only the value
@@ -121,7 +127,6 @@ class RedBlackTree():
                 self.stack.append(direction)
         return None
 
-# TODO: Task 3.
     def tryRedUncle(self):
         x = self.stack.pop()
         x_dir = self.stack.pop()
@@ -146,29 +151,30 @@ class RedBlackTree():
             self.stack.append(x)
             return False
 
-   def repeatRedUncle(self):
-       while self.tryRedUncle():
-           pass
 
-# Provided code to support Task 4:
+    def repeatRedUncle(self):
+        while self.tryRedUncle():
+            pass
 
-    def toNextBlackLevel(self,node):
-        # inspect subtree down to the next level of blacks
+
+    # Provided code to support Task 4: # inspect subtree down to the next level of blacks
+    def toNextBlackLevel(self, node):
         # and return list of components (subtrees or nodes) in L-to-R order
         # (in cases of interest there will be 7 components A,a,B,b,C,c,D).
-        if colourOf(node.left)==Black:  # node.left may be None
+        if colourOf(node.left) == Black:  # node.left may be None
             leftHalf = [node.left]
         else:
             leftHalf = self.toNextBlackLevel(node.left)
-        if colourOf(node.right)==Black:
+        if colourOf(node.right) == Black:
             rightHalf = [node.right]
         else:
             rightHalf = self.toNextBlackLevel(node.right)
         return leftHalf + [node] + rightHalf
 
-    def balancedTree(self,comps):
+
+    def balancedTree(self, comps):
         # build a new (balanced) subtree from list of 7 components
-        [A,a,B,b,C,c,D] = comps
+        [A, a, B, b, C, c, D] = comps
         a.colour = Red
         a.left = A
         a.right = B
@@ -180,23 +186,19 @@ class RedBlackTree():
         b.right = c
         return b
 
+    # TODO: Task 4
 
-# TODO: Task 4
+    #     def endgame(self):
+    #         self.repeatRedUncle()
 
-#     def endgame(self):
-#         self.repeatRedUncle()
+    #   insert(self,key,value)
 
-
-
-#   insert(self,key,value)
-        
-
-# Provided code:
+    # Provided code:
 
     # Printing tree contents
-    
-    def __str__(self,x):
-        if x==None:
+
+    def __str__(self, x):
+        if x == None:
             return 'None:B'
         else:
             leftStr = '[ ' + self.__str__(x.left) + ' ] '
@@ -207,13 +209,13 @@ class RedBlackTree():
         return self.__str__(self.root)
 
     def showStack(self):
-        return [x.__str__() if isinstance(x,Node) else branchLabel(x)
+        return [x.__str__() if isinstance(x, Node) else branchLabel(x)
                 for x in self.stack]
 
     # All keys by left-to-right traversal
-    
-    def keysLtoR_(self,x):
-        if x==None:
+
+    def keysLtoR_(self, x):
+        if x == None:
             return []
         else:
             return self.keysLtoR_(x.left) + [x.key] + self.keysLtoR_(x.right)
@@ -221,25 +223,24 @@ class RedBlackTree():
     def keysLtoR(self):
         return self.keysLtoR_(self.root)
 
+
 # End of class RedBlackTree
 
 
 # Creating a tree by hand:
 sampleTree = RedBlackTree()
-sampleTree.root = Node(2,'two')
+sampleTree.root = Node(2, 'two')
 sampleTree.root.colour = Black
-sampleTree.root.left = Node(1,'one')
+sampleTree.root.left = Node(1, 'one')
 sampleTree.root.left.colour = Black
-sampleTree.root.right = Node(4,'four')
+sampleTree.root.right = Node(4, 'four')
 sampleTree.root.right.colour = Red
-sampleTree.root.right.left = Node(3,'three')
+sampleTree.root.right.left = Node(3, 'three')
 sampleTree.root.right.left.colour = Black
-sampleTree.root.right.right = Node(6,'six')
+sampleTree.root.right.right = Node(6, 'six')
 sampleTree.root.right.right.colour = Black
-sampleTree.plainInsert(5,'five')
+sampleTree.plainInsert(5, 'five')
 print(sampleTree.showStack())
-
-
 
 print(sampleTree.tryRedUncle())
 
@@ -250,7 +251,7 @@ print(sampleTree.tryRedUncle())
 def treeSort(L):
     T = RedBlackTree()
     for x in L:
-        T.insert(x,None)
+        T.insert(x, None)
     return T.keysLtoR()
 
 # End of file
